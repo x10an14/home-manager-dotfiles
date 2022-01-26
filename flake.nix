5256ad9f8b25bfeb78fa5179e854493a8f956ac0
Christian C <x10an14@gmail.com>
2022-01-26 23:29:27 +0100

    Refactor: Make yourself more DRY!

--- flake.nix
+++ flake.nix
@@ -17,11 +17,13 @@
     , nixpkgs-unstable
     , homeManager
     , ...
-  }: {
+  }: let
+    system = "x86_64-linux";
+  in {
     homeConfigurations = {
       x10an14 = homeManager.lib.homeManagerConfiguration {
         # Home-manager 'must-haves' on this level, the rest is in `home.nix` and down...
-        system = "x86_64-linux";
+        system = system;
         homeDirectory = "/home/x10an14";
         username = "x10an14";
         stateVersion = "22.05";
@@ -29,7 +31,7 @@
         # Allow reference/installation of unstable packages with `pkgs.unstable.<package>`
         configuration = {config, pkgs, ...}: let
           overlay-unstable = final: prev: {
-            unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
+            unstable = nixpkgs-unstable.legacyPackages."${system}";
           };
         in {
           nixpkgs = {
